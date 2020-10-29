2b7c0ba66171a2dc91c87748b0a77548a62c68b1
Jane <jane.minhyung.kim@gmail.com>
2020-10-28 21:10:48 -0400

    tried on youtube

---
+++ main.js
@@ -0,0 +1,28 @@
+//'typsed in a bunch of code'
+
+//Comment in here our ideas!
+
+//inspired by TabbyCat!
+
+// social justice but make it cute?
+// and/or environmentalism
+// happy little trees?
+
+// functionalities:
+// turn it into a game
+// maybe 
+
+
+// what sites do you want to work on?
+
+// print out to the console in the browser? 
+
+// find the pumpkin
+// stretch function - if there are any strings "pumpkin" on the page, clicking that works too
+
+
+
+// HACKREACTOR as MVP
+
+const contents = document.getElementById('contents');
+contents.parentNode.removeChild(contents)
